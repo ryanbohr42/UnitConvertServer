484c0d87c59482e4d2f53835a821236259b5b07e
Ryan Bohr <ryanbohr42@hotmail.com>
2018-06-21 13:02:37 -0500

    initialized proj

---
+++ UnitConversionUpdate/UnitConversionUpdate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnitConversionUpdate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
